9b67990a06b9187a9bdacc7c84101dc3227c5b64
melanie <melanie@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2008-04-09 14:50:58 +0000

    übersetzt
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@170 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/helpers/distributions_helper.rb
+++ app/helpers/distributions_helper.rb
@@ -44,13 +44,13 @@ module DistributionsHelper
     "<b>Packages: " + dist.packages.size.to_s + "</b><br/>"
     
     if with_buttons
-     out += (link_to 'Show', dist) + " | " + (link_to 'View Metapackages', distribution_metapackages_path(dist.id)) +\
-      " | " + (link_to 'View all Packages', (distribution_packages_path dist.id))
+     out += (link_to 'Anzeigen', dist) + " | " + (link_to 'Bündel anzeigen', distribution_metapackages_path(dist.id)) +\
+      " | " + (link_to 'Alle Pakete anzeigen', (distribution_packages_path dist.id))
       
     
       if is_admin?
-        out += " | " + (link_to 'Edit', edit_distribution_path(dist)) + " | " +\
-        (link_to 'Destroy', dist, :confirm => 'Are you sure?', :method => :delete)
+        out += " | " + (link_to 'Bearbeiten', edit_distribution_path(dist)) + " | " +\
+        (link_to 'Löschen', dist, :confirm => 'Bist du sicher?', :method => :delete)
       end
     end
     out + "</div>"
