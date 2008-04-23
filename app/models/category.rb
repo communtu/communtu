c80b0a24e8c5324a03ab3e3564173620a5733bae
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2008-04-23 14:58:00 +0000

    Kategorien-Editor
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@191 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ActiveRecord::Base
   acts_as_tree :order => "name"
   has_many :metapackages
   belongs_to :category, :foreign_key => 'parent_id'
+  
+  def parent_name
+    if category.nil? then "" else category.name end
+  end
 end
