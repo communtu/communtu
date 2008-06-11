e11baa3c03d01ee2fcd0d97cfb389fa23a8a427a
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2008-06-11 15:57:11 +0000

    typo
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@254 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of ActiveRecord to incrementally modify your database, and
+# please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your database schema. If you need
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 22) do
+ActiveRecord::Schema.define(:version => 23) do
 
   create_table "base_packages", :force => true do |t|
     t.string   "type"
