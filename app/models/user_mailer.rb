2e1516f1bf90b3de5cef4af8389493e25d9d59f6
toddy <toddy@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2008-10-22 14:26:37 +0000

    Benachrichtigungen angepasst.
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@353 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,14 +1,14 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'  
-    @body[:url]  = "http://localhost:3000/activate/#{user.activation_code}"  
+    @subject    += 'Du hast Dich erfolgreich angemeldet bei'  
+    @body[:url]  = "http://www.communtu.de"  
   end
   
   def activation(user)
     setup_email(user)
-    @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://localhost:3000/"
+    @subject    += 'Dein Benutzerkonto wurde aktiviert.'
+    @body[:url]  = "http://www.communtu.de"
   end
   
   def forgot_password(user)
@@ -19,14 +19,14 @@ class UserMailer < ActionMailer::Base
  
   def reset_password(user)
     setup_email(user)
-    @subject    += 'Your password has been reset.'
+    @subject    += 'Dein Passwort wurde zurueckgesetzt..'
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "mail@yourapplication.com"
-      @subject     = "YourApplication - "
+      @from        = "info@communtu.de"
+      @subject     = "Communtu"
       @sent_on     = Time.now
       @body[:user] = user
     end
