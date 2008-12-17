36c2eb10f642c9762f37f242c708f9eb998b4723
toddy <toddy@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2008-12-17 17:11:58 +0000

    nil objekt entfernt
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@411 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,32 +1,32 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Herzlich Willkommen bei Communtu'  
+    @subject    = 'Herzlich Willkommen bei Communtu'  
     @body[:url]  = "http://www.communtu.de"  
   end
   
   def activation(user)
     setup_email(user)
-    @subject    += 'Dein Benutzerkonto wurde aktiviert.'
+    @subject    = 'Dein Benutzerkonto wurde aktiviert.'
     @body[:url]  = "http://www.communtu.de"
   end
   
   def forgot_password(user)
     setup_email(user)
-    @subject    += 'You have requested to change your password'
+    @subject    = 'You have requested to change your password'
     @body[:url]  = "http://localhost:3000/reset_password/#{user.password_reset_code}"
   end
  
   def reset_password(user)
     setup_email(user)
-    @subject    += 'Dein Passwort wurde zurueckgesetzt..'
+    @subject    = 'Dein Passwort wurde zurueckgesetzt..'
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
       @from        = "info@communtu.de"
-#      @subject     = "Communtu"
+      @subject     = ""
       @sent_on     = Time.now
       @body[:user] = user
     end
