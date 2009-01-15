9d396413c9899be3fb4c8bd7e8200ef2cb568c2f
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-01-15 22:41:05 +0000

    corrected database table relationships
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@507 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,6 +1,5 @@
 class Repository < ActiveRecord::Base
-  has_many :packages, :dependent => :destroy
   belongs_to :distribution
-  has_many :package_distrs
+  has_many :package_distrs, :dependent => :destroy
   validates_presence_of :license_type, :url, :distribution_id
 end
