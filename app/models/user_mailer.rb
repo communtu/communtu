420b2fac48d093e88b5e712f3537410e0bf201bf
toddy <toddy@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-03-30 17:36:31 +0000

    #330
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@621 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,11 +1,8 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
-#    if user.email[-13..-1]!="example.org"
-#    else
     setup_email(user)
     @subject    = 'Herzlich Willkommen bei Communtu'  
     @body[:url]  = "http://www.communtu.de"
-#    end    
   end
   
  # def contact_us(user)  
--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,6 +1,8 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
-    UserMailer.deliver_signup_notification(user)
+    if user.email[-11..-1]!="example.org"
+      UserMailer.deliver_signup_notification(user)
+    end  
   end
 
   def after_save(user)
