e37ef59eea1e02a243379caee7ded031a959fb38
toddy <toddy@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-05-02 13:00:17 +0000

    #310
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@670 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/my_mailer.rb
+++ app/models/my_mailer.rb
@@ -7,7 +7,7 @@ class MyMailer < ActionMailer::Base
   #      @recipients = 'info@communtu.de'
   #      @from = 'info@communtu.de'
   #  else
-        @recipients = 'info@communtu.de', current_user.email
+        @recipients = 'info@communtu.de'
         @from = current_user.email
   #  end
    # @recipients = 'info@communtu.de', current_user.email
