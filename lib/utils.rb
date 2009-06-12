458bfbb376fea99c15b8454578b69441e514e4a7
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-06-12 13:20:42 +0000

    added I18n
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@842 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,5 +1,5 @@
   def safe_system cmd
     if !system cmd
-      raise t(:message_0, :scope => [:txt, :lib, :utils])+cmd
+      raise I18n.t(:message_0, :scope => [:txt, :lib, :utils])+cmd
     end
   end
