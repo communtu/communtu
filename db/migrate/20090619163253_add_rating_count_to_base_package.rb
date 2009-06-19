23a17aaa1dfa7afe73fe509aca8dd14e635a646f
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-06-19 16:41:48 +0000

    missing field
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@878 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

---
+++ db/migrate/20090619163253_add_rating_count_to_base_package.rb
@@ -0,0 +1,9 @@
+class AddRatingCountToBasePackage < ActiveRecord::Migration
+  def self.up
+    add_column :base_packages, :ratings_count, :integer
+  end
+
+  def self.down
+    remove_column :base_packages, :ratings_count
+  end
+end
