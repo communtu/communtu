10d1078458787ef9d30364e0e9f5bcc43091826f
toddy <toddy@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-06-22 14:10:31 +0000

    method name and so on
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@907 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- lib/utils.rb
+++ lib/utils.rb
@@ -3,3 +3,14 @@
       raise I18n.t(:lib_utils_0)+cmd
     end
   end
+
+   def translation(id)
+    tr=Translation.find(:first, :conditions => {:translatable_id => id, :language_code => I18n.locale.to_s })
+    if tr.nil? then
+      tr=Translation.find(:first, :conditions => {:translatable_id => id, :language_code => "en" })
+        if tr.nil? then
+          return "unknown"
+        end
+    end
+    return tr.contents
+  end
