6ce0f81d7bb19ddfa1b3a6c58faf820ab3f80ab7
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-06-22 14:31:27 +0000

    restored from version 892
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@908 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,30 @@
 ActionController::Routing::Routes.draw do |map|
+
+  map.connect '/debs/generate', :controller => 'debs', :action => 'generate'
+  map.connect '/debs/generate_all', :controller => 'debs', :action => 'generate_all'
+  map.resources :debs
+
   map.resources :derivatives
   map.resources :videos
 
   
   map.connect '/categories/show_tree', :controller => 'categories', :action => 'show_tree'
   map.resources :categories
-  map.resources :metapackages
   map.resources :packages
   map.resources :repositories
   
+  map.connect '/metapackages/save', :controller => 'metapackages', :action => 'save'
+  map.connect '/metapackages/immediate_conflicts', :controller => 'metapackages', :action => 'immediate_conflicts'
+  map.connect '/metapackages/conflicts', :controller => 'metapackages', :action => 'conflicts'
+  map.connect '/metapackages/rdepends', :controller => 'metapackages', :action => 'rdepends'
   map.connect '/metapackage/action', :controller => 'metapackages', :action => 'action'
   map.connect '/metapackage/changed', :controller => 'metapackages', :action => 'changed'
   map.connect '/metapackage/migrate', :controller => 'metapackages', :action => 'migrate'
   map.connect '/metapackage/finish_migrate', :controller => 'metapackages', :action => 'finish_migrate'
-  
+
+  map.resources :metapackages
+
+  map.connect '/users/anonymous_login', :controller => 'users', :action => 'anonymous_login'
   map.resources :users, :member => { :enable => :put } do |users|
     users.resource  :user_profile
     users.resource  :account
@@ -24,6 +35,8 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '/users/:id/suggestion/install', :controller => 'suggestion', :action => 'install'
   map.connect '/users/:id/suggestion/install_new', :controller => 'suggestion', :action => 'install_new'
   map.connect '/users/:id/suggestion/install_sources', :controller => 'suggestion', :action => 'install_sources'
+  map.connect '/users/:id/suggestion/install_package_sources/:pid', :controller => 'suggestion', :action => 'install_package_sources'
+  map.connect '/users/:id/suggestion/install_bundle_sources/:mid', :controller => 'suggestion', :action => 'install_bundle_sources'
   map.connect '/users/:id/suggestion/quick_install/:mid', :controller => 'suggestion', :action => 'quick_install'
   map.connect '/users/:id/suggestion/shownew', :controller => 'suggestion', :action => 'shownew'
   map.connect '/users/:user_id/metapackages/:id', :controller => 'users', :action => 'metapackages'
@@ -33,7 +46,8 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '/users/:user_id/user_profile/update_ratings', :controller => 'user_profiles', :action => 'update_ratings'
   map.connect '/users/:user_id/user_profile/refine', :controller => 'user_profiles', :action => 'refine'
   map.connect '/users/:user_id/user_profile/installation', :controller => 'user_profiles', :action => 'installation'
-  map.connect '/users/:user_id/destroy', :controller => 'users', :action => 'destroy'
+  map.connect '/users/:id/destroy', :controller => 'users', :action => 'destroy' 
+  map.connect '/users/:id/show', :controller => 'users', :action => 'show'
   map.connect '/users/:id/cart/:action/:id', :controller => 'cart'
   
   map.resources :distributions do |dist|
@@ -42,6 +56,7 @@ ActionController::Routing::Routes.draw do |map|
     dist.resources :repositories
   end
   
+  map.connect '/packages/rdepends', :controller => 'packages', :action => 'rdepends'
   map.connect 'packages/section', :controller => "packages", :action => "section", :method => :post
   map.connect 'packages/search', :controller => "packages", :action => "search", :method => :post
   map.connect 'packages/update', :controller => "packages", :action => "update", :method => :post
@@ -57,6 +72,9 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '/home',  :controller => 'home', :action => 'home'
   map.connect '/about', :controller => 'home', :action => 'about'
   map.connect '/umfrage', :controller => 'home', :action => 'umfrage'
+  map.connect '/contact_us', :controller => 'home', :action => 'contact_us'
+  map.connect '/cancel', :controller => 'home', :action => 'cancel'
+  map.connect '/success', :controller => 'home', :action => 'success'
   map.connect '/danke/:id', :controller => 'home', :action => 'danke', :method => :get
   map.connect '/admin/load_packages', :controller => 'admins', :action => 'load_packages'
   map.connect '/admin/sync_package/:id', :controller => 'admins', :action => 'sync_package'
@@ -64,7 +82,10 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '/admin/test_all/:id', :controller => 'admins', :action => 'test_all'
   map.connect '/admin/repositories', :controller => 'repositories', :action => 'new'
   map.connect '/distributions', :controller => 'distributions', :action => 'index'
+  map.connect '/rating/rate', :controller => 'rating', :action => 'rate'
 
+  map.resources :sent, :mailbox
+  map.resources :messages, :member => { :reply => :get, :forward => :get }
 
   map.activate '/activate/:id', :controller => 'accounts', :action => 'show'
   map.forgot_password '/forgot_password', :controller => 'passwords', :action => 'new'
@@ -74,7 +95,8 @@ ActionController::Routing::Routes.draw do |map|
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.categories '/categories', :controller => 'categories', :action => 'new' 
-  map.admin '/admin', :controller => 'admin' 
+  map.admin '/admin', :controller => 'admin'
+  map.inbox '/inbox', :controller => "mailbox", :action => "show"
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
