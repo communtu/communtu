24b446bb812f8f049a9887a968f2bbe4f5f688dc
toddy <toddy@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-10-22 21:03:03 +0000

    fix #721
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1222 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -60,5 +60,5 @@ class ApplicationController < ActionController::Base
 #  def set_locale
     ## if params[:locale] is nil then I18n.default_locale will be used
 #    I18n.locale = params[:locale]
-#  end
+#  end         
 end
