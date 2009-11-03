77a76ba9c6b8345894af1ab82b965383168eb40b
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-11-03 14:51:48 +0000

    fixed wrong default value
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1247 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- db/migrate/20091030055614_add_outdated_to_package_distr.rb
+++ db/migrate/20091030055614_add_outdated_to_package_distr.rb
@@ -1,7 +1,7 @@
 class AddOutdatedToPackageDistr < ActiveRecord::Migration
   def self.up
-    add_column :package_distrs, :outdated, :boolean, :default => :false
-    add_column :package_distrs_architectures, :outdated, :boolean, :default => :false
+    add_column :package_distrs, :outdated, :boolean, :default => false
+    add_column :package_distrs_architectures, :outdated, :boolean, :default => false
   end
 
   def self.down
