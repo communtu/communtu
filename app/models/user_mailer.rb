a20c95d53f2c8dafc558d0812e99c17b19d3a8ec
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-11-03 18:30:52 +0000

    corrected email address
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1251 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -35,7 +35,7 @@ class UserMailer < ActionMailer::Base
  def livecd(user,iso)
     @body[:url]  = iso
     @recipients  = "#{user.email}"
-    @from        = "info@communtu.org"
+    @from        = "communtu@gmx.de"
     @subject     = I18n.t(:livecd_email)
     @sent_on     = Time.now
     @body[:user] = user
