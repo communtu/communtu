6f1c73ef2fede6fd99608571bf455885b93ba3db
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-11-11 15:40:43 +0000

    some first unit tests
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1261 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- test/unit/architecture_test.rb
+++ test/unit/architecture_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class ArchitectureTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "browser_architecture_with_default_not_nil" do
+     ["xxxi386xxx","xxxamd64xxx","xxxyyy"].each do |s|
+       assert !Architecture.browser_architecture_with_default(s).nil?
+     end
+  end
+  test "browser_architecture_with_default_i386" do
+     assert Architecture.browser_architecture_with_default("xxx686yyy").name == "i386"
+  end
+  test "browser_architecture_with_default_amd64" do
+     assert Architecture.browser_architecture_with_default("xxx86_64yyy").name == "amd64"
   end
 end
