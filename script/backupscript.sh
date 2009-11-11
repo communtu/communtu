319a212ae4479747f840efa0d4ec0919be56fed3
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-11-11 18:28:28 +0000

    fixed bug causing archive corruption; added script for extracting paths
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1267 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- script/backupscript.sh
+++ script/backupscript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+paths=`cat $HOME/backup.config | tr '\000' '\n' | awk '/IncludeDirectories/ {getline; s=getline; print "" ; while($s ~ /home/) { print " -g "; print $s; s=getline  } }'  |tr -d '\000-\011' |tr -d '\013-\037' | tr '\n' ' '`
 comp="--bzip2=9 -Z \*.zip -Z \*.jpg -Z \*.bz2 -Z \*.gz -Z \*.gif -Z \*.png"
 fping -q 192.168.178.42
 if [ "$?" != "0" ]; then SERVER=bremer-commune.dyndns.org; else SERVER=192.168.178.42; fi
@@ -9,7 +10,9 @@ case $1 in
 		dar -c - -g "$3" -R "/home/$USER/" -v $comp --key $2 | $ssh "cat > `echo $USER`_full_backup_`date -I`.1.dar" 
 		;;
 	diff)
-		$ssh "dar $comp -A \`/bin/ls -1 | grep _full_backup_ |grep $USER | /usr/bin/sort -r | /usr/bin/head -1\` -C - --key-ref $2" > /home/$USER/CAT_$USER.1.dar
+		$ssh "name=\`/bin/ls -1 | grep _full_backup_ |grep $USER | /usr/bin/sort -r | /usr/bin/head -1\`; name2=\`basename \$name .1.dar\`; dar $comp -A \$name2 -C - --key $2 --key-ref $2" > /home/$USER/CAT_$USER.1.dar
 		dar -c - -A /home/$USER/CAT_$USER -v  --key $2 --key-ref $2 -R "/home/$USER/" -g "$3" $comp | $ssh "cat > `echo $USER`_diff_backup_`date -I`.dar"
 		;;
 esac
+
+
