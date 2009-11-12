5d63cd034bbe8f6acd0d6dbbffd2952eedecd54a
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-11-12 10:29:52 +0000

    backup script reads config files
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1270 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- script/backupscript.sh
+++ script/backupscript.sh
@@ -1,18 +1,53 @@
 #!/bin/bash
-date=`date +%Y-%m-%d--%H:%M:%S`
-paths=`cat $HOME/backup.config | tr '\000' '\n' | awk '/IncludeDirectories/ {getline; s=getline; print "" ; while($s ~ /home/) { print " -g "; print $s; s=getline  } }'  |tr -d '\000-\011' |tr -d '\013-\037' | tr '\n' ' '`
-comp="--bzip2=9 -Z \*.zip -Z \*.jpg -Z \*.bz2 -Z \*.gz -Z \*.gif -Z \*.png"
-fping -q 192.168.178.42
+
+#automatic backup script using dar and a remote server
+
+# determine best server address (intranet or internet?)
+ping -q -c 1 192.168.178.42
 if [ "$?" != "0" ]; then SERVER=bremer-commune.dyndns.org; else SERVER=192.168.178.42; fi
-ssh="ssh -i /home/$USER/.ssh/stallman_rsa bc-backup@$SERVER"
+# how to call the server
+# note that you need to install public key authentication, with private key in ~/.ssh/stallman_rsa
+ssh="ssh -i $HOME/.ssh/stallman_rsa bc-backup@$SERVER"
+
+# precise time stamp (in case of more than daily backups)
+date=`date +%Y-%m-%d--%H:%M:%S`
+
+# full or differential backup?
+mode=$1
+
+# read directory list from dar-gui generated config file, if not given as argument
+configfile=$HOME/backup.config
+passwdfile=$HOME/backup.password
+if [ $# -gt 1 ] 
+then
+  passwd=$2
+else
+  passwd=`head -1 $passwdfile`
+fi
+if [ $# -gt 2 ] 
+then
+  paths="-g $3"
+  while [ $# -gt 3 ]; do
+    shift
+    paths=${paths}" -g $3"
+  done
+else
+  paths=`cat $configfile | tr '\000' '\n' | awk '/IncludeDirectories/ {getline; s=getline; print "" ; while($s ~ /home/) { print " -g "; print $s; s=getline  } }'  |tr -d '\000-\011' |tr -d '\013-\037' | tr '\n' ' ' | sed "s/\/home\/${USER}\///g"`
+fi
+
+# do not compress already compressed files
+comp="--bzip2=9 -Z \*.zip -Z \*.jpg -Z \*.bz2 -Z \*.gz -Z \*.gif -Z \*.png"
+
 cd
-case $1 in
+
+# call dar
+case $mode in
 	full)
-		dar -c - -g "$3" -R "/home/$USER/" -v $comp --key $2 | $ssh "cat > backup_${USER}_${date}_full.1.dar" 
+		dar -c - ${paths} -R $HOME -v $comp --key ${passwd} | $ssh "cat > backup_${USER}_${date}_full.1.dar" 
 		;;
 	diff)
-		$ssh "name=\`/bin/ls -1 | grep backup_${USER}_ | /usr/bin/sort -r | /usr/bin/head -1\`; name2=\`basename \$name .1.dar\`; dar $comp -A \$name2 -C - --key $2 --key-ref $2" > /home/$USER/CAT_$USER.1.dar
-		dar -c - -A /home/$USER/CAT_$USER -v  --key $2 --key-ref $2 -R "/home/$USER/" -g "$3" $comp | $ssh "cat > backup_${USER}_${date}_diff.1.dar"
+		$ssh "name=\`/bin/ls -1 | grep backup_${USER}_ | /usr/bin/sort -r | /usr/bin/head -1\`; name2=\`basename \$name .1.dar\`; dar $comp -A \$name2 -C - --key ${passwd} --key-ref ${passwd}" > $HOME/CAT_$USER.1.dar
+		dar -c - -A $HOME/CAT_$USER -v  --key ${passwd} --key-ref ${passwd} -R $HOME ${paths} $comp | $ssh "cat > backup_${USER}_${date}_diff.1.dar"
 		;;
 esac
 
