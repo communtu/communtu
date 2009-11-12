5ca22d3aaf25a49d3f556724f843b4f0a0eaa024
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2009-11-12 09:42:10 +0000

    add exact timestamps; provide multiple differential backups even on the same day
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1269 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- script/backupscript.sh
+++ script/backupscript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+date=`date +%Y-%m-%d--%H:%M:%S`
 paths=`cat $HOME/backup.config | tr '\000' '\n' | awk '/IncludeDirectories/ {getline; s=getline; print "" ; while($s ~ /home/) { print " -g "; print $s; s=getline  } }'  |tr -d '\000-\011' |tr -d '\013-\037' | tr '\n' ' '`
 comp="--bzip2=9 -Z \*.zip -Z \*.jpg -Z \*.bz2 -Z \*.gz -Z \*.gif -Z \*.png"
 fping -q 192.168.178.42
@@ -7,11 +8,11 @@ ssh="ssh -i /home/$USER/.ssh/stallman_rsa bc-backup@$SERVER"
 cd
 case $1 in
 	full)
-		dar -c - -g "$3" -R "/home/$USER/" -v $comp --key $2 | $ssh "cat > `echo $USER`_full_backup_`date -I`.1.dar" 
+		dar -c - -g "$3" -R "/home/$USER/" -v $comp --key $2 | $ssh "cat > backup_${USER}_${date}_full.1.dar" 
 		;;
 	diff)
-		$ssh "name=\`/bin/ls -1 | grep _full_backup_ |grep $USER | /usr/bin/sort -r | /usr/bin/head -1\`; name2=\`basename \$name .1.dar\`; dar $comp -A \$name2 -C - --key $2 --key-ref $2" > /home/$USER/CAT_$USER.1.dar
-		dar -c - -A /home/$USER/CAT_$USER -v  --key $2 --key-ref $2 -R "/home/$USER/" -g "$3" $comp | $ssh "cat > `echo $USER`_diff_backup_`date -I`.dar"
+		$ssh "name=\`/bin/ls -1 | grep backup_${USER}_ | /usr/bin/sort -r | /usr/bin/head -1\`; name2=\`basename \$name .1.dar\`; dar $comp -A \$name2 -C - --key $2 --key-ref $2" > /home/$USER/CAT_$USER.1.dar
+		dar -c - -A /home/$USER/CAT_$USER -v  --key $2 --key-ref $2 -R "/home/$USER/" -g "$3" $comp | $ssh "cat > backup_${USER}_${date}_diff.1.dar"
 		;;
 esac
 
