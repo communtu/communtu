35fb681ca03693d1aceb6e3bb8d0bde249d21a20
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2010-01-12 16:24:17 +0000

    also use subcategories for selection of default bundles
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1441 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ActiveRecord::Base
   acts_as_tree :order => "name"
   has_many :metapackages
   belongs_to :category, :foreign_key => 'parent_id'
+  has_many :categories, :foreign_key => 'parent_id'
 
   def name
     translation(self.name_tid)
@@ -23,7 +24,16 @@ class Category < ActiveRecord::Base
   def parent_name
     if category.nil? then "" else category.name end
   end
-  
+
+  def children
+    categories
+  end
+
+  def recursive_children
+    ch = children
+    ([self] + ch + ch.map{|c| c.recursive_children}.flatten).uniq
+  end
+
   def self.draw_tree
     f=File.open("categories.dot","w")
     f.puts "digraph G {"
@@ -41,6 +51,8 @@ class Category < ActiveRecord::Base
   end
 
   def default_bundles
-    BasePackage.find_all_by_default_install_and_category_id(true,self.id)
+    (recursive_children.map do |c|
+      BasePackage.find_all_by_default_install_and_category_id(true,c.id)
+    end).flatten
   end
 end
