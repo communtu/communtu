81c7a139db71aead8adad29af550aeb143ec5a18
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2010-01-19 16:22:28 +0000

    fixed bug in remaster call
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1459 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/livecd.rb
+++ app/models/livecd.rb
@@ -59,7 +59,7 @@ class Livecd < ActiveRecord::Base
       else
         virt = ""
       end
-      remaster_call = "sudo -u communtu #{RAILS_ROOT}/script/remaster create #{virt} #{ver} #{iso} #{isobase} #{srcdeb} #{installdeb} >> #{RAILS_ROOT}/log/livecd.log 2>&1"
+      remaster_call = "sudo -u communtu #{RAILS_ROOT}/script/remaster create #{virt} #{ver} #{iso} #{self.name} #{self.srcdeb} #{self.installdeb} >> #{RAILS_ROOT}/log/livecd.log 2>&1"
       system "echo \"#{remaster_call}\" >> #{RAILS_ROOT}/log/livecd.log"
       res = system remaster_call
     else
