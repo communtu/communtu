b4f3e88d6a3ad050a0f2da4faa01074d80a5d0d4
timo <timo@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2010-05-08 12:10:25 +0000

    Testing if multiple values exists works.
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1751 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- script/check_duplicates.py
+++ script/check_duplicates.py
@@ -3,10 +3,7 @@
 '''
 Author: Timo Denissen
 E-Mail: timo@communtu.com
-About this program: This program is for creating new and editing
-                    existing *.gpx files at the computer for using
-                    them with the Java application "TrekBuddy".
-Version: 0.1A.0
+About this program: 
 Todo: - Check template.yml for double values *WIP*
       - If double entries exist, ask the user which to use and delete
         the other value(s)
@@ -17,23 +14,46 @@ Todo: - Check template.yml for double values *WIP*
 
 import yaml
 
+#Working with the file.
 def arbeit():
     mydict = yaml.load(template_yml)
-    print mydict
+#    print mydict
+#    print type(mydict)
     template_yml.close()
-    print 'File closed.'
+    new_dict = switch_keyvalue(mydict['de'])
+#    print new_dict
+    check_dict(new_dict)
+    print 'Exit'
 
+# Check if there are multiple values for one key.
+def check_dict(dict):
+    for key, value in dict.iteritems():
+        if len(value) > 1:
+            print 'Found the following duplicate values:'
+            print value, ':', key
+        else:
+            pass
+    
+# Switching the key:value pairs in the template.yml file.
+def switch_keyvalue(dict):
+    new_dict = {}
+    for key, value in dict.iteritems():
+        if value in new_dict:
+            new_dict[value].append(key)
+        else:
+            new_dict[value] = [key]
+    return new_dict
+    
 # Asking which file to work with. Either default or a file chosen by the user.
-print 'This program is pre-alpha and not meant for even testing purposes!'
-print 'File loss may occur!'
+print 'This program is beta'
 use_file = raw_input('Use "config/locales/template.yml"? y/n ')
 if use_file == "y":
     template_yml = open('config/locales/template.yml')
-    print template_yml
+#    print template_yml
     arbeit()
 elif use_file == "n":
     template_yml = open(raw_input('Which file to use? '))
-    print template_yml
+#    print template_yml
     arbeit()
 else:
     print "Exiting"
