c4941c5eabbd0578435643ab20c5d824978c64cd
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2010-05-29 19:00:39 +0000

    reading control file: next try
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1810 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/deb.rb
+++ app/models/deb.rb
@@ -388,7 +388,7 @@ class Deb < ActiveRecord::Base
     # get dependencies from control file
     f.read.each do |line|
       if !(ind=line.index("Depends: ")).nil?
-        actual_deps = Set.new(line[ind+9,line.length].split(", ").map{|n| if n[-1]=32 then n.chop else n end})
+        actual_deps = Set.new(line[ind+9,line.length].split(", ").map{|n| if n[-1]==32 then n.chop else n end})
         needed_deps = Set.new(self.dependencies(arch))
         if actual_deps==needed_deps
           return "correct"
