268863147109ec5f046e7288f0a7967639b3a60e
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2010-06-06 13:27:31 +0000

    fixed #1166
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1839 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/models/livecd.rb
+++ app/models/livecd.rb
@@ -181,7 +181,7 @@ class Livecd < ActiveRecord::Base
     end
   end
 
-  MSGS = ["Failed to fetch","could not set up","Cannot install","is not installable","not going to be installed", "Depends:"]
+  MSGS = ["Failed to fetch","could not set up","Cannot install","is not installable","not going to be installed", "Depends:","rror","annot"]
   
   def short_log
     if log.nil?
