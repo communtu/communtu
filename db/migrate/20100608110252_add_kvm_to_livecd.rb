211d423f8f418e4de4bddacb87a92780d906593b
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2010-06-08 19:18:44 +0000

    added iso field
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1851 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- db/migrate/20100608110252_add_kvm_to_livecd.rb
+++ db/migrate/20100608110252_add_kvm_to_livecd.rb
@@ -1,10 +1,12 @@
 class AddKvmToLivecd < ActiveRecord::Migration
   def self.up
+    add_column :livecds, :iso, :boolean, :default => false
     add_column :livecds, :kvm, :boolean, :default => false
     add_column :livecds, :usb, :boolean, :default => false
   end
 
   def self.down
+    remove_column :livecds, :iso
     remove_column :livecds, :kvm
     remove_column :livecds, :usb
   end
