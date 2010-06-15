4c5fafa1e02050b17094408ceffd9aaa74c0f0eb
timo <timo@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2010-06-15 20:33:29 +0000

    code optimizing, started to work on edit_strings() function for manual string naming
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@1936 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- script/check_duplicates.py
+++ script/check_duplicates.py
@@ -12,9 +12,7 @@ import os
 
 def file_processing():
     '''Working with the file.'''
-    mydict = yaml.safe_load(template_yml)
-    template_yml.close()
-    new_dict = switch_keyvalue(mydict['de'])
+    new_dict = switch_keyvalue(template_dict)
     check_dict(new_dict)
     
 def check_dict(dict):
@@ -34,13 +32,12 @@ def ask_string(string, key):
     x = 1
     for item in string:
         temp_dict[x] = item
-        print x, item
+        print x , item
         x += 1
     print '0', 'Enter a new name.'
     ask_number = input('Enter the number of the correct key: ')
     if ask_number == 0:
         newkey = raw_input('Enter a new name: ')
-        print key
         add_to_template(newkey, key)
     else:
         newkey = temp_dict[ask_number]
@@ -50,13 +47,9 @@ def ask_string(string, key):
     
 def add_to_template(user_string, new_value):
     '''Add the new key to the template.yml'''
-    template_yml = open('config/locales/template.yml')
-    template_dict_full = yaml.safe_load(template_yml)
-    template_dict = template_dict_full['de']
-    template_yml.close()
     template_yml = open('config/locales/template.yml', 'w')
     template_dict[user_string] = new_value
-    yaml.dump_all([template_dict_full], stream=template_yml, default_flow_style=False, width=2048, line_break=False, allow_unicode=True, explicit_start=True)
+    safe_file(template_dict_full, template_yml)
 
 def edit_files(oldkeys, newkey):
     '''Using "sed" to remove oldkey from source files.'''
@@ -74,14 +67,9 @@ def edit_files(oldkeys, newkey):
 
 def edit_template(key):
     '''Editing the template.yml and removing multiple key:value pairs.'''
-    template_yml = open('config/locales/template.yml')
-    template_dict_full = yaml.safe_load(template_yml)
-    template_dict = template_dict_full['de']
-    template_yml.close()
     template_yml = open('config/locales/template.yml', 'w')
     del template_dict[key]
-    yaml.dump_all([template_dict_full], stream=template_yml, default_flow_style=False, width=2048, line_break=False, allow_unicode=True, explicit_start=True)
-    template_yml.close()
+    safe_file(template_dict_full, template_yml)
 
 def switch_keyvalue(dict):
     '''Switching the key:value pairs in the template.yml file.'''
@@ -92,9 +80,45 @@ def switch_keyvalue(dict):
         else:
             new_dict[value] = [key]
     return new_dict
-
-'''Choosing the file to work with.'''
-print 'Using file "config/locales/template.yml"'
-template_yml = open('config/locales/template.yml')
-file_processing()
-print 'Exit'
+    
+def safe_file(dictionary, stream_file):
+    '''Saving the file using yaml.dump_all'''
+    yaml.dump_all([dictionary], stream=stream_file, default_flow_style=False, width=2048, line_break=False, allow_unicode=True, explicit_start=True)
+    stream_file.close()
+    
+def open_file():
+    global template_dict_full
+    global template_dict
+    template_yml = open('config/locales/template.yml')
+    print 'Using file "config/locales/template.yml"'
+    template_dict_full = yaml.safe_load(template_yml)
+    template_dict = template_dict_full['de']
+    template_yml.close()
+    
+def edit_strings():
+    '''Manual editing of strings'''
+    search = raw_input('Enter the search string: ')
+    print '\nSearching for string "' + search +'".\n'
+    cache = {}
+    count = 0
+    for key, value in template_dict.items():
+        if search in key:
+            count += 1
+            cache[count] = [key, value]
+            print count, key + ':', value
+    print '\nFound string', '"' + search + '"', count, 'times.\n'
+    ask_number = input('Which string do you want to edit? ')
+    newkey = cache[ask_number]
+    print newkey
+    
+'''Asking the user what to do'''
+open_file()
+print '\nWhat do you want to do?\n1. Check for multiple entries\n2. Edit strings\n0. Exit\n'
+menu = raw_input('What do you want to do? ')
+if menu == '1':
+    file_processing()
+elif menu == '2':
+#    edit_strings()
+    print 'Not working yet.'
+else:
+    print 'Exit'
