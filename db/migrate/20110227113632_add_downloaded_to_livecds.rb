bb486e1f111a0ec4b51a9e75c5f7fab336e170de
commune <commune@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2011-03-03 21:10:21 +0000

    trace migration
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@2243 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- db/migrate/20110227113632_add_downloaded_to_livecds.rb
+++ db/migrate/20110227113632_add_downloaded_to_livecds.rb
@@ -1,12 +1,16 @@
 class AddDownloadedToLivecds < ActiveRecord::Migration
   def self.up
-    add_column :livecds, :downloaded, :int, :default => 0
+#    add_column :livecds, :downloaded, :int, :default => 0
     Dir.glob("/var/log/apache2/*-communtu.log*gz").each do |file|
       IO.popen("gunzip -c #{file}").each do |line|
         cdname = line.scan(/.*\/isos\/(.*).iso.*/).flatten[0]
-        Livecd.all.select{|cd| cd.fullname == cdname}.each do |cd|
-          cd.downloaded += 1
-          cd.save
+        if !cdname.nil?
+          puts cdname
+          Livecd.all.select{|cd| cd.fullname == cdname}.each do |cd|
+            cd.downloaded += 1
+            cd.save
+            puts cd.name
+          end
         end
       end
     end
