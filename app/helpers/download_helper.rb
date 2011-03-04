ea16471b15f9bb44932869a86bd59ccc60627fc2
till <till@d8b24053-1f83-4035-b5c3-ce8b92cd91ad>
2011-03-04 08:49:32 +0000

    made fix for #1433 more efficient
    git-svn-id: file:///home/communtu/karsten/svn/communtu-program@2249 d8b24053-1f83-4035-b5c3-ce8b92cd91ad

--- app/helpers/download_helper.rb
+++ app/helpers/download_helper.rb
@@ -10,37 +10,44 @@ module DownloadHelper
     end
 
   
-    def paths
-    {"install" => [{:name => I18n.t(:download_area), :action => "start"},
-             {:name => I18n.t(:model_user_profile_tabz_1), :action => "selection"},
-             {:name => I18n.t(:model_user_profile_tabz_4), :action => "settings"},
-             {:name => I18n.t(:add_sources), :action => "prepare_install_sources"},
-             {:name => I18n.t(:controller_suggestion_15), :action => "installation"}],
-             "cd" => [{:name => I18n.t(:download_area), :action => "start"},
-             {:name => I18n.t(:model_user_profile_tabz_1), :action => "selection"},
-             {:name => I18n.t(:model_user_profile_tabz_4), :action => "settings"},
-             {:name => I18n.t(:create_livecd), :action => "livecd"},
-             {:name => I18n.t(:wait_for_email), :action => "cd_email"},
-             {:name => I18n.t(:cd_ready), :action => "current_cd"}],
-             "bundle_cd" => [{:name => I18n.t(:model_user_meta_tabz_3), :action => "/metapackages"},
-             {:name => I18n.t(:model_user_profile_tabz_4), :action => "/download/settings"},
-             {:name => I18n.t(:bundle_to_livecd1), :action => "/download/bundle_to_livecd"},
-             {:name => I18n.t(:wait_for_email), :action => "/download/create_livecd_from_bundle"},
-             {:name => I18n.t(:cd_ready), :action => "/download/current_cd"}],
-             "mybundle_cd" => [{:name => I18n.t(:model_user_meta_tabz_0), :action => "/metapackages/index_mine"},
-             {:name => I18n.t(:model_user_profile_tabz_4), :action => "/download/settings"},
-             {:name => I18n.t(:bundle_to_livecd1), :action => "/download/bundle_to_livecd"},
-             {:name => I18n.t(:wait_for_email), :action => "/download/create_livecd_from_bundle"},
-             {:name => I18n.t(:cd_ready), :action => "/download/current_cd"}],
-             "usb" => [{:name => I18n.t(:download_area), :action => "start"},
-             {:name => I18n.t(:model_user_profile_tabz_1), :action => "selection"},
-             {:name => I18n.t(:model_user_profile_tabz_4), :action => "settings"},
-             {:name => I18n.t(:create_iso), :action => "livecd"},
-             {:name => I18n.t(:wait_for_email), :action => "cd_email"},
-             {:name => I18n.t(:iso_ready), :action => "current_cd"}
-            # {:name => "USB-Stick erstellen", :action => "usb"}
-       ]
-    }
+    def paths(p)
+      case p
+        when "install" 
+        then [{:name => I18n.t(:download_area), :action => "start"},
+              {:name => I18n.t(:model_user_profile_tabz_1), :action => "selection"},
+              {:name => I18n.t(:model_user_profile_tabz_4), :action => "settings"},
+              {:name => I18n.t(:add_sources), :action => "prepare_install_sources"},
+              {:name => I18n.t(:controller_suggestion_15), :action => "installation"}]
+        when "cd" 
+        then [{:name => I18n.t(:download_area), :action => "start"},
+              {:name => I18n.t(:model_user_profile_tabz_1), :action => "selection"},
+              {:name => I18n.t(:model_user_profile_tabz_4), :action => "settings"},
+              {:name => I18n.t(:create_livecd), :action => "livecd"},
+              {:name => I18n.t(:wait_for_email), :action => "cd_email"},
+              {:name => I18n.t(:cd_ready), :action => "current_cd"}]
+        when "bundle_cd"
+        then [{:name => I18n.t(:model_user_meta_tabz_3), :action => "/metapackages"},
+              {:name => I18n.t(:model_user_profile_tabz_4), :action => "/download/settings"},
+              {:name => I18n.t(:bundle_to_livecd1), :action => "/download/bundle_to_livecd"},
+              {:name => I18n.t(:wait_for_email), :action => "/download/create_livecd_from_bundle"},
+              {:name => I18n.t(:cd_ready), :action => "/download/current_cd"}]
+        when "mybundle_cd"
+        then [{:name => I18n.t(:model_user_meta_tabz_0), :action => "/metapackages/index_mine"},
+              {:name => I18n.t(:model_user_profile_tabz_4), :action => "/download/settings"},
+              {:name => I18n.t(:bundle_to_livecd1), :action => "/download/bundle_to_livecd"},
+              {:name => I18n.t(:wait_for_email), :action => "/download/create_livecd_from_bundle"},
+              {:name => I18n.t(:cd_ready), :action => "/download/current_cd"}]
+        when "usb"
+        then [{:name => I18n.t(:download_area), :action => "start"},
+              {:name => I18n.t(:model_user_profile_tabz_1), :action => "selection"},
+              {:name => I18n.t(:model_user_profile_tabz_4), :action => "settings"},
+              {:name => I18n.t(:create_iso), :action => "livecd"},
+              {:name => I18n.t(:wait_for_email), :action => "cd_email"},
+              {:name => I18n.t(:iso_ready), :action => "current_cd"}
+             # {:name => "USB-Stick erstellen", :action => "usb"}
+             ]
+        else nil
+      end  
   end           
   
 
