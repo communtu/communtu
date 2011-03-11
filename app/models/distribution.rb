0f1209f3fd16bead40a2e53710b10b8183d3690a
Till Mossakowski <till@communtu.de>
2011-03-11 01:38:35 +0100

    added AGPL license

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -1,5 +1,18 @@
-# (c) 2008-2010 by Verein Allgemeinbildung e.V., Bremen, Germany
-# use, modification or distribution only with permission of the copyright holder
+# (c) 2008-2011 byllgemeinbildung e.V., Bremen, Germany
+# This file is part of Communtu.
+
+ # Communtu is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU Affero Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+
+ # Communtu is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU Affero Public License for more details.
+
+ # You should have received a copy of the GNU Affero Public License
+ # along with Communtu.  If not, see <http://www.gnu.org/licenses/>.
 
 # Ubuntu distributions like Karmic or Lucid
 # Communtu allows the contents of bundles to depend on the distribution
