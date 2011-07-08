9e51cf0469ea3df7deb348a1ab931bc989db0096
Mihai Codescu <Mihai.Codescu@dfki.de>
2011-07-08 11:43:40 +0200

    fixing ticket 1558

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -115,7 +115,7 @@ class UsersController < ApplicationController
   end
 
   
-  def update
+  def update 
     @user = User.find(current_user)
     flash[:notice] = t(:controller_users_5)
     if @user.anonymous then
