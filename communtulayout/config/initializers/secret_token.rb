215e3083e9a7b08b306132647f1866686fe2768e
Till Mossakowski <till@communtu.de>
2012-01-08 16:01:30 +0100

    new rails project for redesign of Communtu in the coding dojo

---
+++ communtulayout/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Communtulayout::Application.config.secret_token = '7562cb6ac88e79e4fc5c2fe32a579e64654c28a9097f8f67310064dab8e36a1a7134abcd76d2490ece8fd1100ec9e204821780677086221bbb34f67912cd0018'
