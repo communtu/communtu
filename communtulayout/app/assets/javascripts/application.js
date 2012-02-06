000c367089fa0be48e00d59cab792f016bdbf41a
Till Mossakowski <till@communtu.de>
2012-02-06 19:46:16 +0100

    dojo 6th Feb 2012

--- communtulayout/app/assets/javascripts/application.js
+++ communtulayout/app/assets/javascripts/application.js
@@ -7,3 +7,25 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+function rotate(i){
+		$("#start" + i).animate({opacity:1},3000);
+		document.getElementById("start"+i).style.display="block";
+		
+		$("#start" + (i+2)%3).animate({opacity:0},3000);
+		s="document.getElementById('start"+(i+2)%3+"').style.display='none'";
+		window.setTimeout(s,3000);
+		i += 1;
+		if(i>2){
+			i=0;
+	    }
+		window.setTimeout("rotate("+i+")",10000);  
+} 
+
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
