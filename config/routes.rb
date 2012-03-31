34155365351bee9621a559c747e2fd5f97727774
Torsten Franz <torsten@communtu.de>
2012-03-31 21:28:35 +0200

    fixed 1602 - switch route from index to start

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '/download/create_livecd/:id', :controller => 'download', :action => 'create_livecd'
   map.connect '/download/test_livecd/:id', :controller => 'download', :action => 'test_livecd'
   map.connect '/download/bundle_to_livecd', :controller => 'download', :action => 'bundle_to_livecd'
+  map.connect '/download/index', :controller => 'download', :action => 'start'
 
   map.connect '/users/anonymous_login', :controller => 'users', :action => 'anonymous_login'
   map.resources :users, :member => { :enable => :put, :search => :put, :anonymous_login => :get} do |users|
